6fc327c414becdedae8aa3289def640aeaa12d61
IceJackal <jack098y@gmail.com>
2015-07-14 12:16:17 +0100

    Update Sieve of Eratosthenes.py

--- Sieve of Eratosthenes.py
+++ Sieve of Eratosthenes.py
@@ -1,8 +1,7 @@
-def SieveOfEratosthenes(limit):
+def SieveOfEratosthenes0(limit):
     numbers = [x for x in range(2, limit + 1)] 
     primes = [True] * (limit - 1)
     results = []
-
     p = 2 
     for x in numbers:
         if primes[x - 2] == True:
@@ -13,11 +12,21 @@ def SieveOfEratosthenes(limit):
                 else: 
                     primes[(num * p) - 2] = False 
         else:
-            pass 
-
+            pass
     for x in numbers: 
         if primes[x - 2] == True:
             results.append(x)
 
 
     return results
+
+
+def SieveOfEratosthenes1(limit):
+    limit += 1
+    primes = [True] * limit
+    primes[0] = primes[1] = False
+    for i in range(2, int(limit**0.5)):
+        if primes[i]:
+            for j in range(i+i, limit, i):
+                primes[j] = False
+    return [x for x in range(limit) if primes[x]]
